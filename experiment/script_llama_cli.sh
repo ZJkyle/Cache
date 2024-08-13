2153307664c80051f1f6fd95ccb3153cebb9be2f
ZJkyle <zhijiakyle@gmail.com>
2024-08-13 12:57:59 +0000

    TODO: perplexity

--- experiment/script_llama_cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# 執行指令
-./../llama-cli -m ../../models/Meta-Llama-3.1-8B-Instruct/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf -p "I believe the meaning of life is" -n -2 -t 12 -c 256 --keep -1
